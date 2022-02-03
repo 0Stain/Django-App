8e01e7cc232c8db65e2e99ae8e00807776e2ce41
louayjeddou <48138944+0Stain@users.noreply.github.com>
2022-02-03 17:03:16 +0100

    Added Models
    Functionning app (added models and setup)

--- DjangoProject/DjangoApp/models.py
+++ DjangoProject/DjangoApp/models.py
@@ -1,3 +1,63 @@
+from importlib.util import set_loader
+from pydoc import describe
 from django.db import models
+from django.core.validators import MinValueValidator
+
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()  # de base charfield
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Student(User):
+    pass
+
+
+class Coach(User):
+    pass
+
+
+class Projet(models.Model):
+    project_name = models.CharField(max_length=50)
+    dure = models.IntegerField()
+    temps_allocated = models.IntegerField(validators=[MinValueValidator(1, "Minimum time = 1")])
+    besoin = models.TextField(max_length=250)
+    description = models.TextField(max_length=250)
+    isValid = models.BooleanField(default=False)
+    creator = models.OneToOneField(
+        Student,
+        on_delete=models.CASCADE,
+        related_name='creators'
+                                   )
+    supervisor = models.ForeignKey(
+        Coach,
+        on_delete=models.CASCADE,
+        related_name='supervisors'
+    )
+    membre = models.ManyToManyField(
+        Student,
+        # on_delete=models.CASCADE,
+        through='MemberShip',
+        related_name='membres'
+    )  # classe intermediaire nommé memberShip
+
+
+class MemberShip(models.Model):
+    projet = models.ForeignKey(
+        Projet,
+        on_delete=models.CASCADE,
+        related_name='projets'
+                               )
+    student = models.ForeignKey(
+        Student,
+        on_delete=models.CASCADE,
+        related_name='students'
+                                )
+
+    temps_allocated_by_member = models.IntegerField(default=0)
